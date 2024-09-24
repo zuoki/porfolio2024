d73cd03eea932af7fd89077f3e92eb9eb264b650
zuoki <jgcorrea56@gmail.com>
2024-09-24 15:35:37 -0300

    porfolio 0.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
 
   return (
-    <div className='bg-gradient-to-bl from-yellow-400 to-violet-700'>
+    <div className='bg-gradient-to-bl from-red-800 to-violet-700'>
       <NavBar/>
       <Me />
       <Table/>
