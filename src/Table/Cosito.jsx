92b266e97960026eb920a4bcb1664e3a600a4cc1
zuoki <jgcorrea56@gmail.com>
2024-09-25 16:36:03 -0300

    porfolio 0.2.2

--- src/Table/Cosito.jsx
+++ src/Table/Cosito.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Cosito = () => {
   return (
-    <div className='w-full h-5 bg-orange-700 flex justify-end' >
-        <img src="/sonido.png" alt=""  className='h-10 mt-[-2px] z-20'/>
+    <div className='w-full h-5 bg-orange-700  ' >
+        <img src="/sonido.png" alt=""  className=' relative h-10 mt-[-2px] z-20 ml-[85vw] sm:ml-[75vw] xl:ml-[70vw] 2xl:ml-[63vw]'/>
     </div>
   )
 }
